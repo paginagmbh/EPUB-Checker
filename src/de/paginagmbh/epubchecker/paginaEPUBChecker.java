969d5023c29748b976b6ee999d6773a7f453234d
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-12-09 22:41:22 +0100

    Final release v1.6.0

--- src/de/paginagmbh/epubchecker/paginaEPUBChecker.java
+++ src/de/paginagmbh/epubchecker/paginaEPUBChecker.java
@@ -26,15 +26,15 @@ import com.apple.eawt.QuitResponse;
  * @author		Tobias Fischer
  * @copyright	pagina GmbH, Tübingen
  * @version		1.6.0
- * @date			2015-12-05
+ * @date			2015-12-09
  */
 public class paginaEPUBChecker {
 
 	// +++++++++++++++++++++++++ DON'T FORGET TO UPDATE EVERYTIME ++++++++++++++++++ //
 
 	public static final String PROGRAMVERSION = "1.6.0";
-	public static final String VERSIONDATE = "05.12.2015";
-	public static final String PROGRAMRELEASE = "beta";	// "" or "beta"
+	public static final String VERSIONDATE = "09.12.2015";
+	public static final String PROGRAMRELEASE = "";	// "" or "beta"
 	public static final String RELEASENOTES = "- EpubCheck library v4.0.1<br/>- Fixed Linux support<br/>- Fixed Java JRE support on Mac OS X<br/>- Other bugfixes and improvements";
 
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
